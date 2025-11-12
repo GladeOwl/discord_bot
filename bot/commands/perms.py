3e1848591f2608dfe0ee821d0423df62343822ee
GladeOwl <owloftheglade@gmail.com>
2025-11-12 17:43:35 +0530

    Implement logging

--- bot/commands/perms.py
+++ bot/commands/perms.py
@@ -1,8 +1,8 @@
+import logging
 import hikari
 import lightbulb
-from lightbulb.prefab import checks
 
-from core import CLIENT
+LOGGER = logging.getLogger("bot")
 
 @lightbulb.hook(lightbulb.ExecutionSteps.CHECKS)
 async def fail_if_not_officer(_:lightbulb.ExecutionPipeline, ctx: lightbulb.Context) -> None:
@@ -12,5 +12,6 @@ async def fail_if_not_officer(_:lightbulb.ExecutionPipeline, ctx: lightbulb.Cont
         return
     
     if hikari.Permissions.ADMINISTRATOR not in member.permissions:
+        logging.info(f"{ctx.member} tried to use the command {ctx.command}, but lacked the appropriate roles.")
         await ctx.respond("You lack the required permissions to execute this command. Please ding an Officer for help.", ephemeral=True)
         raise lightbulb.prefab.checks.MissingRequiredPermission # pyright: ignore[reportAttributeAccessIssue]
