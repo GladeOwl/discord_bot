2d07729d5a663ef7c30e3a47f34c373b4c78afba
GladeOwl <owloftheglade@gmail.com>
2025-11-12 16:07:34 +0530

    Implement perms for Roles

--- bot/commands/roles.py
+++ bot/commands/roles.py
@@ -1,6 +1,8 @@
 import hikari
 import lightbulb
+
 from typing import Mapping
+from commands import perms
 from core import CLIENT
 
 @CLIENT.register
@@ -8,21 +10,22 @@ class ReplaceRole(
     lightbulb.SlashCommand,
     name="replacerole",
     description="Find everybody who has role_a, and replace it with role_b.",
+    hooks=[perms.fail_if_not_officer]
 ):
-    
     roleA: hikari.Role = lightbulb.role("role_a", "Find everybody with this role.")
     roleB: hikari.Role  = lightbulb.role("role_b", "Give them this role and remove the other one.")
 
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
         await edit_role(self.roleA, self.roleB, ctx, remove=True)
-        await ctx.respond("...", ephemeral=True)
+        
 
 @CLIENT.register
 class AddRole(
     lightbulb.SlashCommand,
     name="addrole",
-    description="Find everybody who has a role_a, give them role_b.",
+    description="Find everybody who has a role_a, also give them role_b.",
+    hooks=[perms.fail_if_not_officer]
 ):
     roleA: hikari.Role = lightbulb.role("role_a", "Find everybody with this role.")
     roleB: hikari.Role = lightbulb.role("role_b", "Give them this role.")
@@ -30,16 +33,13 @@ class AddRole(
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
         await edit_role(self.roleA, self.roleB, ctx, remove=False)
-        await ctx.respond("...", ephemeral=True)
 
 async def edit_role(roleA, roleB, ctx: lightbulb.Context, remove: bool) -> None:
     member: hikari.Member | None = ctx.member
-    print(member)
     if member is None:
         return
     
     guild: hikari.Guild | None = member.get_guild()
-    print(guild)
     if guild is None:
         return
     
@@ -47,11 +47,25 @@ async def edit_role(roleA, roleB, ctx: lightbulb.Context, remove: bool) -> None:
     if members is None:
         return
     
+    processed_members: int = 0
+    members_with_role: int = 0
     for member in members.values():
         if roleA not in member.get_roles():
             continue
-
-        await member.add_role(roleB)
+        
+        if roleB not in member.get_roles():
+            await member.add_role(roleB)
+            processed_members += 1 if not remove else 0
+        else:
+            members_with_role += 1
 
         if remove:
+            processed_members += 1
             await member.remove_role(roleA)
+
+    if remove:
+        await ctx.respond(f"{roleA.mention} role was replaced with the {roleB.mention} role for {processed_members} member(s).", ephemeral=True)
+    else:
+        message = f"{roleB.mention} role was given to {processed_members} member(s) that also had the {roleA.mention} role."
+        message = message + f" {members_with_role} member(s) already had the role." if members_with_role > 0 else ""
+        await ctx.respond(message, ephemeral=True)
